54b40702bd2be5f0db9c2345d7035a614da70a15
Ariful islam Masum <arifulislammasum97@gmail.com>
2025-09-15 00:33:17 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     Scaffold(
+    return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: const Center(child: Text('Welcome to the Home Screen!')),
     );
